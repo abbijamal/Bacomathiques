5cb765e92c24cb8dad65afb4156b5bf54dc1f9b2
Skyost <Skyost@users.noreply.github.com>
2021-06-11 00:19:42 +0000

    deploy: ee191420c44525f45d9b0365da861a4b1e44b043

---
+++ _nuxt/static/1623370756/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623370756",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ad150a14"}}}}(null,"\u002F"));
