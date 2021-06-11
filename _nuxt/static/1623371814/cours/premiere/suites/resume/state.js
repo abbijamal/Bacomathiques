bde0570b619317a44be7f510383bf63d9526e9c1
Skyost <Skyost@users.noreply.github.com>
2021-06-11 23:14:18 +0000

    deploy: ec807bfafa2e7687fe08b4ca5163c3769dc02d0f

--- _nuxt/static/1623371814/cours/premiere/suites/resume/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623371814",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcours\u002Fpremiere\u002Fsuites\u002Fresume",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5a72e095"}}}}(null));
