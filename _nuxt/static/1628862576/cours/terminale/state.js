425bad52f1b80e7a11005eec481ac587fe35639e
Skyost <Skyost@users.noreply.github.com>
2021-08-13 14:36:04 +0000

    deploy: 723a9dd393a509bf52356156bf8e89d141576424

--- _nuxt/static/1628862576/cours/terminale/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628862576",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcours\u002Fterminale",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"32c1a8d3"}}}}(null));
